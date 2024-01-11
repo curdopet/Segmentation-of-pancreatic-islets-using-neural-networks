57d926d99d2bc4f341d41c19ca7a10924a50c23b
curdopet <curdovape@gmail.com>
2024-01-11 20:10:09 +0100

    update dependencies for local environment

--- training/local_scripts/install_environment.sh
+++ training/local_scripts/install_environment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-conda install -c conda-forge progress -y
-conda install -c conda-forge wandb -y
+conda install -c conda-forge progress=1.6 wandb=0.16.2 plotly=5.18.0 jupyter=1.0.0 jupyter_server=1.23.4 scikit-learn=1.3.0 -y
 
 pip install -U openmim
 mim install mmengine
